ebae09ff85a0dbe1095c61616f2590f464331452
Rakesh Lohan <lohan.rakesh@gmail.com>
2020-06-25 14:25:22 +0530

    Product Detail

---
+++ app/src/main/java/com/example/shopping/ProductDetail.kt
@@ -0,0 +1,34 @@
+package com.example.shopping
+
+import android.R
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
+
+
+class ProductDetail : AppCompatActivity() {
+
+    var sampleImages = intArrayOf(
+        R.drawable.image_1,
+        R.drawable.image_2
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+
+        val carouselView = findViewById(R.id.carouselView) as CarouselView;
+        carouselView.setPageCount(sampleImages.size);
+        carouselView.setImageListener(imageListener);
+    }
+
+
+    var imageListener: ImageListener = object : ImageListener {
+        override fun setImageForPosition(position: Int, imageView: ImageView) {
+            // You can use Glide or Picasso here
+            imageView.setImageResource(sampleImages[position])
+        }
+    }
+}
